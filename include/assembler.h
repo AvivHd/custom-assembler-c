439a8ecfaf1131c51acdf81136ee31e28230a3d7
Aviv Hadar <avivhadar4@gmail.com>
2026-02-02 12:30:16 +0200

    Add assembler source code

---
+++ include/assembler.h
@@ -0,0 +1,16 @@
+/* Aviv Hadar, ID: 207750993
+ * This program is the header of the assembler. */
+
+#ifndef MAMAN14_PROJ_ASSEMBLER_H
+#define MAMAN14_PROJ_ASSEMBLER_H
+
+#include "globals.h"
+#include "parser.h"
+#include "output.h"
+#include "firstpass.h"
+#include "sec_pass.h"
+#include "encode.h"
+
+#endif
+
+
